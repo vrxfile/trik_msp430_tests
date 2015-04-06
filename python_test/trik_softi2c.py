7a0f5147339003ade85df8323313f98f568731fa
vrxfile <vrxfile@mail.ru>
2015-04-06 18:54:04 +0300

    Added functions to test soft I2C

--- python_test/trik_softi2c.py
+++ python_test/trik_softi2c.py
@@ -27,16 +27,21 @@ i2c_enable = 0x80
 i2c_read = 0x20
 i2c_write = 0x40
 i2c_sens = 0x10
+i2c_pull = 0x08
 
 # Sensor type
 nxttemp = 0x0000
 hmc5883l_x = 0x0001
 hmc5883l_y = 0x0002
 hmc5883l_z = 0x0003
+mcp3424_ch1 = 0x0004
+mcp3424_ch2 = 0x0005
+mcp3424_ch3 = 0x0006
+mcp3424_ch4 = 0x0007
 
 # Enable I2C
 def enable_i2c(i2cnum):
-    trik_protocol.write_reg(i2cnum, ictl, i2c_enable)
+    trik_protocol.write_reg(i2cnum, ictl, i2c_enable + i2c_pull)
 
 # Disable I2C
 def disable_i2c(i2cnum):
@@ -47,7 +52,7 @@ def read_i2c(i2cnum, i2cdev, i2creg):
     trik_protocol.write_reg(i2cnum, idev, i2cdev)
     trik_protocol.write_reg(i2cnum, ireg, i2creg)
     trik_protocol.write_reg(i2cnum, idat, 0x00)
-    trik_protocol.write_reg(i2cnum, ictl, i2c_enable + i2c_read)
+    trik_protocol.write_reg(i2cnum, ictl, i2c_enable + i2c_pull + i2c_read)
     return trik_protocol.read_reg(i2cnum, idat)
 
 # Write I2C device register
@@ -55,23 +60,21 @@ def write_i2c(i2cnum, i2cdev, i2creg, i2cdat):
     trik_protocol.write_reg(i2cnum, idev, i2cdev)
     trik_protocol.write_reg(i2cnum, ireg, i2creg)
     trik_protocol.write_reg(i2cnum, idat, i2cdat)
-    trik_protocol.write_reg(i2cnum, ictl, i2c_enable + i2c_write)
+    trik_protocol.write_reg(i2cnum, ictl, i2c_enable + i2c_pull + i2c_write)
 
 # Set I2C sensor type
 def set_i2c_sensor_type(i2cnum, i2csenstype):
     trik_protocol.write_reg(i2cnum, iidx, i2csenstype)
 
 # Read I2C sensor
-def read_i2c(i2cnum):
-    trik_protocol.write_reg(i2cnum, ictl, i2c_enable + i2c_sens)
+def read_i2c_sensor(i2cnum):
+    trik_protocol.write_reg(i2cnum, ictl, i2c_enable + i2c_pull + i2c_sens)
     return trik_protocol.read_reg(i2cnum, ival)
 
-"""
 # Get SCTL register
-def get_sensor_control(sensnum):
-    return trik_protocol.read_reg(sensnum, sctl)
+def get_i2c_control(i2cnum):
+    return trik_protocol.read_reg(i2cnum, ictl)
 
 # Get SIDX register (type of sensor)
-def get_sensor_type(sensnum):
-    return trik_protocol.read_reg(sensnum, sidx)
-"""
+def get_i2_sensor_type(i2cnum):
+    return trik_protocol.read_reg(i2cnum, iidx)
